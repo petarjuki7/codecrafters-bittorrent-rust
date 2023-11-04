29822db04f850dd2a3da97c51581f5607f32e5f5
Petar Jukic <petar.jukic7@gmail.com>
2023-11-04 21:43:12 +0100

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,13 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    if let Some((len, string)) = encoded_value.split_once(':') {
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(string[..len].to_string());
+        }
+    } 
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -26,12 +23,12 @@ fn main() {
 
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
+        //println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+         let encoded_value = &args[2];
+         let decoded_value = decode_bencoded_value(encoded_value);
+         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
